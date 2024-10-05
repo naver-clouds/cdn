2f3fa02ad0add6de8327312e5fc2728036fc88da
enkiark <96149033+enkiark@users.noreply.github.com>
2024-10-05 23:03:24 +0900

    Update google.js

--- google.js
+++ google.js
@@ -5,7 +5,6 @@ $('.dwonle > ul > li').each(function(idx, li) {
 var product = $(this).attr('onclick')
 var newproduct = product.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
 
-console.log(newproduct)
 $(this).attr('onclick',newproduct)
 })
 
