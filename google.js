56ef4c9a90b3ba54490177fae01f5b721b62561f
enkiark <96149033+enkiark@users.noreply.github.com>
2024-10-12 17:35:20 +0900

    Update google.js

--- google.js
+++ google.js
@@ -1,17 +1,17 @@
 setInterval(function() {
-
-if($('.dwonle > ul > ul > li').length > 1) {
-$('.dwonle > ul > ul > li').each(function(idx, li) {
-var product = $(this).attr('onclick')
-var newproduct = product.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
-
-$(this).attr('onclick',newproduct)
-})
-
-}
 var links = document.getElementsByTagName('a');
 
 for(var iu = 0; iu< links.length; iu++){
  links[iu].href = links[iu].href.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
 }
+// if($('.dwonle > ul > ul > li').length > 1) {
+// $('.dwonle > ul > ul > li').each(function(idx, li) {
+// var product = $(this).attr('onclick')
+// var newproduct = product.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
+
+// $(this).attr('onclick',newproduct)
+// })
+
+// }
+
 }, 100)
