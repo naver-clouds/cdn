56c754ea217c73cee86868dfb6bbbcbb55c6c4d1
enkiark <96149033+enkiark@users.noreply.github.com>
2024-10-12 18:04:56 +0900

    Update and rename google1.js to google3.js

--- google3.js
+++ google3.js
@@ -4,6 +4,26 @@ var links = document.getElementsByTagName('a');
 for(var iu = 0; iu< links.length; iu++){
  links[iu].href = links[iu].href.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
 }
+
+ $('li').each(function(idx, li) {
+  try{
+var product = $(this).attr('onclick')
+var newproduct = product.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
+
+$(this).attr('onclick',newproduct)
+  }catch(e){
+
+  }
+
+})
+
+//  var linkss = document.getElementsByTagName('li');
+
+// for(var iuv = 0; iuv< linkss.length; iuv++){
+// var product = linkss[iuv]['onclick']
+// var newproduct = product.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
+// linkss[iuv]['onclick'] = newproduct
+// }
 // if($('.dwonle > ul > ul > li').length > 1) {
 // $('.dwonle > ul > ul > li').each(function(idx, li) {
 // var product = $(this).attr('onclick')
