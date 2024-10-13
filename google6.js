e1ec54f2eeb09bff5954e23fa3555f53f3b6d1e5
enkiark <96149033+enkiark@users.noreply.github.com>
2024-10-13 16:49:02 +0900

    Update google6.js

--- google6.js
+++ google6.js
@@ -1,12 +1,6 @@
 setInterval(function() {
   try{
-var links = document.getElementsByTagName('a');
-
-for(var iu = 0; iu< links.length; iu++){
- links[iu].href = links[iu].href.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
-}
-//https://drive-usercontent.gooele.org/download?id=1eQlx4RrA2_vKiYhgMN3zJWE7ER9kb0Hc&export=download&authuser=0
- $('li').each(function(idx, li) {
+     $('li').each(function(idx, li) {
 
 var product = $(this).attr('onclick')
    if(product.includes("buly")){
@@ -20,6 +14,14 @@ $(this).attr('onclick',newproduct)
 
 
 })
+    
+var links = document.getElementsByTagName('a');
+
+for(var iu = 0; iu< links.length; iu++){
+ links[iu].href = links[iu].href.replace("drive.usercontent.google.com","drive-usercontent.gooele.org")
+}
+//https://drive-usercontent.gooele.org/download?id=1eQlx4RrA2_vKiYhgMN3zJWE7ER9kb0Hc&export=download&authuser=0
+
   }catch(e){
 
   }
